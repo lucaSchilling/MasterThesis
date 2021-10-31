eb6242e9e61ccfc0c2e5df6d293d9d53b0ae5d31
Luca BigC Server <lucaschilling96@web.de>
2021-10-31 13:23:48 +0000

    added validation.py

--- validation.py
+++ validation.py
@@ -8,6 +8,38 @@ import SimpleITK as sitk
 import numpy as np
 from scipy import stats
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def meshgridnd_like(in_img, rng_func=range):
+    new_shape = list(in_img.shape)
+    all_range = [rng_func(i_len) for i_len in new_shape]
+    return tuple([x_arr.swapaxes(0, 1) for x_arr in np.meshgrid(*all_range)])
+
+
+def get_displacement_3d(displacement):
+    displacement_np = sitk.GetArrayFromImage(displacement)
+    DS_FACTOR = 16
+    c_xx, c_yy, c_zz = [
+        x.flatten() for x in meshgridnd_like(
+            displacement_np[::DS_FACTOR, ::DS_FACTOR, ::DS_FACTOR, 0])
+    ]
+
+    get_flow = lambda i: displacement_np[::DS_FACTOR, ::DS_FACTOR, ::DS_FACTOR,
+                                         i].flatten()
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.gca(projection='3d')
+
+    ax.quiver(c_xx,
+              c_yy,
+              c_zz,
+              get_flow(0),
+              get_flow(1),
+              get_flow(2),
+              length=0.5,
+              normalize=True)
+
 
 def get_landmarks(fixed_image_path: str,
                   indexing: str = 'zyx') -> (np.array, np.array):
@@ -21,6 +53,10 @@ def get_landmarks(fixed_image_path: str,
         # swap columns because numpy and vxm use zyx indexing and the data uses xyz indexing
         moving_landmarks[:, [0, 2]] = moving_landmarks[:, [2, 0]]
         fixed_landmarks[:, [0, 2]] = fixed_landmarks[:, [2, 0]]
+    if indexing == 'yxz':
+        # swap columns because numpy and vxm use zyx indexing and the data uses xyz indexing
+        moving_landmarks[:, [0, 1]] = moving_landmarks[:, [1, 0]]
+        fixed_landmarks[:, [0, 1]] = fixed_landmarks[:, [1, 0]]
     else:
         assert indexing == 'xyz', f'indexing can only be xyz or zyx. Got: {indexing}'
     return moving_landmarks.astype(np.float64), fixed_landmarks.astype(
@@ -64,10 +100,24 @@ def get_moved_points(points: np.array, displacement: sitk.Image) -> np.array:
     return moved_points
 
 
+def get_moved_fixed_checkerboard(framework, moving_image_path,
+                                 fixed_image_path):
+    moving_image = sitk.ReadImage(moving_image_path)
+    fixed_image = sitk.ReadImage(fixed_image_path)
+    moved_image, displacement, time = framework.register_images(
+        fixed_image=fixed_image,
+        moving_image=moving_image,
+        weights_path=model_path,
+        loss=loss)
+    checkerboard = sitk.CheckerBoard(moved_image, fixed_image, [10, 10, 1])
+    return checkerboard
+
+
 framework_name = 'vxmtf'
 dataset = 'synthetic'
-model_path = '/home/lschilling/PycharmProjects/image_registration_thesis/models/test_model_30_100_8batch'
+model_path = '/home/lschilling/PycharmProjects/MasterThesis/models/synthetic/vxmtf_ep200_st100_lr0_001_bat8_final_loss0_0004/weights.h5'
 model_name = os.path.basename(model_path)
+loss = 'MSE'
 if framework_name == 'vxmtf':
     from frameworks.VoxelmorphTF import VoxelmorphTF
     from frameworks.VoxelmorphTorch import VoxelmorphTorch
@@ -95,59 +145,88 @@ if dataset == 'synthetic':
     tre_non_reg_list = []
 time_list = []
 mse_list = []
+mse_non_reg_list = []
 jacobian_reflections_list = []
+checkerboard = None
 for (idx, _) in enumerate(moving_image_paths):
     moving_image = sitk.ReadImage(moving_image_paths[idx])
     fixed_image = sitk.ReadImage(fixed_image_paths[idx])
+    moving_image = sitk.Cast(moving_image, sitk.sitkFloat32)
+    fixed_image = sitk.Cast(fixed_image, sitk.sitkFloat32)
     moved_image, displacement, time = framework.register_images(
-        fixed_image, moving_image, weights_path=model_path)
+        fixed_image=fixed_image,
+        moving_image=moving_image,
+        weights_path=model_path,
+        loss=loss)
     jacobian = get_jacobian_np(displacement)
     jacobian_reflections_list.append(len(jacobian[jacobian < 0]))
     time_list.append(time)
-    mse_list.append(get_mse(moving_image, fixed_image))
+    mse_list.append(get_mse(moved_image, fixed_image))
+    mse_non_reg_list.append(get_mse(moving_image, fixed_image))
+    if idx == 0:
+        checkerboard = sitk.CheckerBoard(moved_image, fixed_image, [10, 10])
+        sitk.Show(checkerboard)
     if dataset == 'synthetic':
         moving_landmarks, fixed_landmarks = get_landmarks(
-            fixed_image_paths[idx], indexing='zyx')
-        moving_landmarks_xyz, fixed_landmarks_xyz = get_landmarks(
             fixed_image_paths[idx], indexing='xyz')
-        moved_landmarks = framework.get_moved_points(fixed_landmarks,
-                                                     displacement)
-        moved_landmarks_sitk = get_moved_points(fixed_landmarks_xyz,
-                                                displacement)
-        tre_test = get_tre(moving_landmarks_xyz, moved_landmarks_sitk)
-        print(stats.describe(tre_test))
-        tre_non_reg_test = get_tre_non_reg(moving_landmarks_xyz,
-                                           fixed_landmarks_xyz)
-        print(stats.describe(tre_non_reg_test))
-
+        moved_landmarks = get_moved_points(fixed_landmarks, displacement)
         tre = get_tre(moving_landmarks, moved_landmarks)
         tre_non_reg = get_tre_non_reg(moving_landmarks, fixed_landmarks)
         tre_list.append(tre.mean())
         tre_non_reg_list.append(tre_non_reg.mean())
-        print(stats.describe(tre))
-        print(stats.describe(tre_non_reg))
         pass
 
-output_path = os.path.join(
-    '/home/lschilling/PycharmProjects/MasterThesis/models', dataset,
-    (model_name if model_name else framework_name))
+output_path = os.path.dirname(model_path)
 if not os.path.exists(output_path):
     os.mkdir(output_path)
 
+sitk.WriteImage(checkerboard,
+                os.path.join(output_path, 'result_checkerboard.nrrd'))
+mse_list_np = np.array(mse_list)
+
 results_dict = {
     'time_list': time_list,
-    'mse_list': mse_list,
-    'jacobian_reflections_list': jacobian_reflections_list,
     'time_mean': np.array(time_list).mean(),
-    'mse_mean': np.array(mse_list).mean(dtype=float),
-    'jacobian_reflections_mean': np.array(jacobian_reflections_list).mean()
+    'mse_non_reg_list': mse_non_reg_list,
+    'mse_non_reg_mean': np.array(mse_non_reg_list).mean(dtype=float),
+    'jacobian_reflections_list': jacobian_reflections_list,
+    'jacobian_reflections_mean': np.array(jacobian_reflections_list).mean(),
+    'mse_list': mse_list,
+    'mse_mean': mse_list_np.mean(dtype=float),
+    'mse_min': np.min(mse_list_np),
+    'mse_max': np.max(mse_list_np),
+    'mse_std': np.std(mse_list_np),
+    'mse_25': np.percentile(mse_list_np, 25),
+    'mse_50': np.percentile(mse_list_np, 50),
+    'mse_75': np.percentile(mse_list_np, 75),
+    'mse_var': np.var(mse_list_np)
 }
 
 if dataset == 'synthetic':
+    tre_list_np = np.array(tre_list)
+
+    sitk.WriteImage(
+        get_moved_fixed_checkerboard(framework,
+                                     moving_image_paths[tre_list_np.argmin()],
+                                     fixed_image_paths[tre_list_np.argmin()]),
+        os.path.join(output_path, 'tre_min_checkerboard.nrrd'))
+
+    sitk.WriteImage(
+        get_moved_fixed_checkerboard(framework,
+                                     moving_image_paths[tre_list_np.argmax()],
+                                     fixed_image_paths[tre_list_np.argmax()]),
+        os.path.join(output_path, 'tre_max_checkerboard.nrrd'))
     results_dict['tre_list'] = tre_list
     results_dict['tre_non_reg_list'] = tre_non_reg_list
-    results_dict['tre_mean'] = np.array(tre_list).mean()
+    results_dict['tre_mean'] = tre_list_np.mean()
     results_dict['tre_non_reg_mean'] = np.array(tre_non_reg_list).mean()
+    results_dict['tre_min'] = np.min(tre_list_np)
+    results_dict['tre_max'] = np.max(tre_list_np)
+    results_dict['tre_std'] = np.std(tre_list_np)
+    results_dict['tre_25'] = np.percentile(tre_list_np, 25)
+    results_dict['tre_50'] = np.percentile(tre_list_np, 50)
+    results_dict['tre_75'] = np.percentile(tre_list_np, 75)
+    results_dict['tre_var'] = np.var(tre_list_np)
 
 json.dump(results_dict,
           open(os.path.join(output_path, 'results.json'), 'w'),
